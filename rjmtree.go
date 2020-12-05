513bb250b4bc1000acda1778bea92861191629bb
rjm <1824793405@qq.com>
2020-12-05 18:22:20 +0800

    now this program's output looks pretty nice. but it still need to improve.

--- rjmtree.go
+++ rjmtree.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 func main() {
@@ -19,36 +18,52 @@ func main() {
 		args = os.Args[1:]
 	}
 	for _, path := range args {
-		if err := mytree(path, 0); err != nil {
+		if err := mytree(path, ""); err != nil {
 			log.Printf("could not tree this path: %s :%v", path, err)
 		}
 	}
 }
 
-func mytree(root string, depth int) error {
+func mytree(root, indent string) error {
+
 	stat, err := os.Stat(root)
 	if err != nil {
 		return errors.Annotatef(err, "could not stat %s", root)
 	}
 
-	// don't tree the ignore file or ignore directories
-	if stat.Name()[0] == '.' {
-		return nil
-	}
-
-	fmt.Printf("%s%s\n", strings.Repeat("  ", depth), stat.Name())
+	fmt.Println(stat.Name())
 
 	// if root is not a directory which means it's a leaf node we return
+	// else we recursively call our tree func
 	if !stat.IsDir() {
 		return nil
 	}
-	// if root is a dirctory then we recursively call tree
-	fs, err := ioutil.ReadDir(root)
+
+	// read all dirs and files in that root directory
+	ds, err := ioutil.ReadDir(root)
 	if err != nil {
 		return errors.Annotatef(err, "could not read dir %s", stat.Name())
 	}
-	for _, v := range fs {
-		if err := mytree(filepath.Join(root, v.Name()), depth+1); err != nil {
+	// put everything in that fs but not ingore file or ingore directory
+	var fs []string
+	for _, d := range ds {
+		if d.Name()[0] != '.' {
+			fs = append(fs, d.Name())
+		}
+	}
+
+	for i, v := range fs {
+		add := "│  "
+		// last child we print this pattern
+		if i == len(fs)-1 {
+			// v don't have a little brother any more
+			fmt.Printf(indent + "└──")
+			add = "   "
+		} else {
+			// this v has brothers
+			fmt.Printf(indent + "├──")
+		}
+		if err := mytree(filepath.Join(root, v), indent+add); err != nil {
 			return err
 		}
 	}
